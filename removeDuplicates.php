cf60345c8fde472d0189c0136637e8e2102e33f9
iurecastro <55067724+iurecastro@users.noreply.github.com>
2023-12-12 23:58:31 +0000

    Update removeDuplicates.php
       //26. Remove Duplicates from Sorted Array

--- removeDuplicates.php
+++ removeDuplicates.php
@@ -5,6 +5,7 @@ class Solution {
      * @param Integer[] $nums
      * @return Integer
      */
+    //26. Remove Duplicates from Sorted Array
     function removeDuplicates(&$nums) {
         $nums = array_unique($nums);
         return count(array_unique($nums));
